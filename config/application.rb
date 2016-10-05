25dd4883864c58f5d536a6a48de048344aae8e7b
NgocLinhNguyen <ngoclinhnguyen.hust@gmail.com>
2016-10-05 14:39:22 +0700

    Create home controller

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +11,7 @@ module MiniSocialNetworking
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2|otf)$)
   end
 end
