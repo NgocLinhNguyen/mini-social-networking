ad88f786cc5fcf826ff9724e4f338752b819db02
longbb <longbuiba95@gmail.com>
2016-12-21 13:20:36 +0700

    Load more when scroll

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,15 @@ class PostsController < ApplicationController
   before_action :post_must_belongs_to_current_user, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.filter_by_user(params[:user_id])
+    @posts = Post.filter_by_user(params[:user_id]).order(created_at: :desc).paginate(page: params[:page], per_page: 5)
+    @posts_info = Array.new
+    @posts.each do |post|
+      post_info = render_to_string(template: "shared/_post.html.haml", locals: { post: post }, layout: false)
+      @posts_info.push post_info
+    end
+    respond_to do |format|
+      format.json
+    end
   end
 
   def show
@@ -62,12 +70,12 @@ class PostsController < ApplicationController
   end
 
   private
-  def post_must_belongs_to_current_user
-    @user = User.find params[:user_id]
-    @post = Post.find params[:id]
-    unless @user.id == current_user.id || @post.user_id == current_user.id
-      flash[:warning] = "Permission denied"
-      redirect_to user_path(current_user)
+    def post_must_belongs_to_current_user
+      @user = User.find params[:user_id]
+      @post = Post.find params[:id]
+      unless @user.id == current_user.id || @post.user_id == current_user.id
+        flash[:warning] = "Permission denied"
+        redirect_to user_path(current_user)
+      end
     end
-  end
 end
